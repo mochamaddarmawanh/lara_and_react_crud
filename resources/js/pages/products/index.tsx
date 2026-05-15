7bfd18bb856671832e1c9f99e18f61243e121fa2
Mochamad Darmawan Hardjakusumah <mochamaddarmawanh@gmail.com>
2026-05-15 21:27:25 +0700

    first commit: sidebar and index product

---
+++ resources/js/pages/products/index.tsx
@@ -0,0 +1,26 @@
+import AppLayout from '@/layouts/app-layout';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: '/dashboard',
+    },
+    {
+        title: 'Products',
+        href: '/products',
+    },
+];
+
+export default function ProductIndex() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Products" />
+
+            <div>
+                Product
+            </div>
+        </AppLayout>
+    );
+}
